176df14c2a7e0e73cd63c9262dbcd276605d112a
Ajal <ajapoos1999@gmail.com>
2020-02-18 14:21:40 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+//setting routes
+const data_routes = require("./routes/volunteer.route");
+
+// Establish a database connection
+mongoose.connect('mongodb://localhost/dhishna');
+
+//Create server
+const server = express();
+
+//Use assets
+server.use("/assets/css", express.static(__dirname + "/assets/css"));
+server.use("/volunteer",data_routes);
+
+//Setting view engine
+server.set("view engine","ejs");
+
+//Show static page
+server.get('/',(req,res) => {
+        res.render('index');
+});
+
+server.listen(3000);
---
+++ models/volunteer.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let volunteerSchema = new Schema ({
+    name : {type : String},
+    phone : {type : String},
+    event : {type : String},
+});
+
+module.exports = mongoose.model('volunteer',volunteerSchema);
